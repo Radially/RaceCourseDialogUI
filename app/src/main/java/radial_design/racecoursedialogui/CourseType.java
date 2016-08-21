d016757cc8596fbd869ed5a3428d688252465448
Radially <jonathanfriedman2@gmail.com>
2016-08-21 23:55:03 +0300

    distance calculation works. xml parser parses  list of CourseType. TODO: legs and options interactions. HIGH: race course RV gone

--- app/src/main/java/radial_design/racecoursedialogui/CourseType.java
+++ app/src/main/java/radial_design/racecoursedialogui/CourseType.java
@@ -1,36 +1,33 @@
 package radial_design.racecoursedialogui;
 
+import android.util.Log;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Jonathan on 13/07/2016.
  */
-public class CourseTypeOptions {
+public class CourseType {
     private String name;
     private int imageID = R.drawable.racecourse_optimist;
     private List<String[]> options; //{name, view to contain options, option1, option2, ...}
-    private Map<String, Double> lengthFactorMap = new HashMap<String, Double>();  //the length of the first leg, divide by all leg length
+    public double[] courseFactors = {1,1,0};
 
 
-    public CourseTypeOptions(String name, List<String[]> options) {
-        lengthFactorMap.put("default", 0.5);
+    public CourseType(String name, List<String[]> options) {
         this.name = name;
         this.options = options;
     }
 
-    public CourseTypeOptions(String name, String[] optionsArray) {
-        lengthFactorMap.put("default", 0.5);
+    public CourseType(String name, String[] optionsArray) {
         List<String[]> options = new ArrayList<>();
         options.add(optionsArray);
         this.name = name;
         this.options = options;
     }
 
-    public CourseTypeOptions(String name) {
-        lengthFactorMap.put("default", 0.5);
+    public CourseType(String name) {
         this.name = name;
         this.options = null;
     }
@@ -56,13 +53,22 @@ public class CourseTypeOptions {
         return options;
     }
 
-    /*public double getDistance(Map<String,String> selectedOptions, double avarageVMG, int targetTime){
-        try{
-             return lengthFactorMap.get(selectedOptions.get("Legs"))*avarageVMG/targetTime;
-        }
-        catch (Exception e){
-            Log.w("CourseTypeOptions:" ,"no length factor");
-            return 0.5;
-        }
-    }*/
+    public void setOptions(List<String[]> options) {
+        this.options = options;
+    }
+
+    public void setCourseFactors(double[] courseFactors) {
+        this.courseFactors = courseFactors;
+    }
+
+    public double[] getCourseFactors() {
+        return courseFactors;
+    }
+
+    public void setCourseFactor(int index, double factor){
+        if(index<courseFactors.length)courseFactors[index]=factor;
+        else Log.w("CourseType class", "setCourseFactor: out of factors");
+
+    }
+
 }
